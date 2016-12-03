7a285ae40a20447a279807ef1629514de2ec2290
Derek Lo <dlo102789@gmail.com>
2016-12-03 13:32:50 -0600

    Version 1.0 - Added menus, improved layout and functionality

--- js/script.js
+++ js/script.js
@@ -1,18 +1,54 @@
 var numSquares = prompt("Enter a number between 16 and 64")
 
 $(document).ready(function() {
-	for(var i = 0; i < numSquares*numSquares; i++) {
+	drawGrid(numSquares);
+	var drawColor = 'grey'
+	draw(drawColor)
+
+	$('#colors').on('change', function() {
+		$('.tile').css("background-color","white").removeClass("grey black blue red yellow").off()
+		drawColor = $('#colors option:selected').val()
+		console.log(drawColor)
+		switch(drawColor) {
+			case 'black':
+				draw('black')
+				break
+			case 'blue':
+				draw('blue')
+				break
+			case 'red':
+				draw('red')
+				break
+			case 'yellow':
+				draw('yellow')
+				break
+		}
+	})
+
+	$('#pickSize').on("click", function() {
+		$('.container').empty()
+		numSquares = prompt("Enter a number between 16 and 64")
+		drawGrid(numSquares)
+		draw(drawColor)
+	})
+
+})
+
+var draw = function(color) {
+	$('.tile').mouseenter(function() {
+		$(this).addClass(color)
+	})
+}
+
+var drawGrid = function(num) {
+
+	for(var i = 0; i < num*num; i++) {
 		$('.container').append('<div class="tile"></div>')
 	}
 
 	$('.tile').css({
-		'width': $('.container').width()/numSquares,
-		'height': $('.container').height()/numSquares
+		'width': $('.container').width()/num,
+		'height': $('.container').height()/num
 	})
-
-	$('.tile').mouseenter(function() {
-		$(this).addClass("grey")
-	})
-
-})
+}
 
